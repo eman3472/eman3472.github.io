94a11d3f58774c71d7465fa2a79a52a960e2c885
eamin129 <firstlastemail19@gmail.com>
2023-10-28 00:30:57 -0400

    you can now purchase equipment
    see title

--- js/basic.js
+++ js/basic.js
@@ -258,3 +258,98 @@ function testSaveCookie()
     setCookie("test","wow",3650);
     console.log("Cookie Saved");
 }
+
+var infEq = 0;
+var supEq = 0;
+var tanks = 0;
+var fjets = 0;
+var bjets = 0;
+var bships = 0;
+
+function PurchaseEQ(id)
+{
+    switch(id)
+    {
+        case 0:
+            if(pp >= 10)
+            {
+                pp -= 10;
+                infEq += 10;
+            }
+            else
+            {
+                console.log("You poor");
+            }
+            document.getElementById("inf").innerHTML = infEq + " Infantry Equipment";
+            break;
+
+        case 1:
+            if(pp >= 10)
+            {
+                pp -= 10;
+                supEq += 10;
+            }
+            else
+            {
+                console.log("You poor");
+            }
+            document.getElementById("sup").innerHTML = supEq + " Support Equipment";
+            break;
+
+        case 2:
+            if(pp >= 100)
+            {
+                pp -= 100;
+                tanks++;
+            }
+            else
+            {
+                console.log("You poor");
+            }
+            document.getElementById("tank").innerHTML = tanks + " Tanks";
+            break;
+
+        case 3:
+            if(pp >= 250)
+            {
+                pp -= 250;
+                fjets++;
+            }
+            else
+            {
+                console.log("You poor");
+            }
+            document.getElementById("fjet").innerHTML = fjets + " Fighter Jets";
+            break;
+        
+        case 4:
+            if(pp >= 260)
+            {
+                pp -= 260;
+                bjets++;
+            }
+            else
+            {
+                console.log("You poor");
+            }
+            document.getElementById("bjet").innerHTML = bjets + " Bomber Jets";
+            break;
+        
+        case 5:
+            if(pp >= 350)
+            {
+                pp -= 350;
+                bships++;
+            }
+            else
+            {
+                console.log("You poor");
+            }
+            document.getElementById("bship").innerHTML = bships + " Battleships";
+            break;
+
+            default:
+                console.log("You messed up");
+    }
+    UpdateUI();
+}
