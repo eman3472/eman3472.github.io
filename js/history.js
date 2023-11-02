1d7b467eee026657c3008d152a254f08e3df7347
eamin129 <firstlastemail19@gmail.com>
2023-11-01 23:04:51 -0400

    added log file to debug cookies

--- js/history.js
+++ js/history.js
@@ -12,9 +12,10 @@
         default:
             break;
     }
+    console.log(Number(getCookie("pureDay")));
 }
 
 function AppendEvents(id)
 {
-    document.getElementById("events").innerHTML += "<button class='button button2'>Fuck</button>\n";
+    document.getElementById("events").innerHTML += "<button class='button button2'>Test</button>\n";
 }
